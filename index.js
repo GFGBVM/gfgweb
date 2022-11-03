3fc7804def4d3d45661c4d9aa0e318b588aca01e
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2022-11-03 15:36:30 +0530

    added contact form section

--- index.js
+++ index.js
@@ -37,3 +37,41 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 // Events end
+
+// Contacts start
+var iframe = document.getElementById("contact-form");
+iframe.scrolling = "no";
+
+const checkActive = () => {
+    console.log("ran");
+    if (document.activeElement.id == "contact-form")
+        iframe.scrolling = "yes";
+}
+
+var observer = new IntersectionObserver(function (entries) {
+    if (entries[0].isIntersecting === true) {
+        window.addEventListener('blur', checkActive);
+    } else {
+        window.removeEventListener('blur', checkActive);
+        iframe.scrolling = "no";
+    }
+}, { threshold: [0] });
+
+observer.observe(document.querySelector("#contact-form"));
+// Contacts end
+
+
+const pageLoaded = () => {
+  
+  // Remove loader
+  const loader = document.getElementById('loader_block');
+  loader.style.opacity = '0';
+  setTimeout(() => loader.style.display = 'none', 300);
+
+  // Lazy load contact form iframe
+  document.getElementById('contact-form').setAttribute('src', 'https://docs.google.com/forms/d/e/1FAIpQLSfF5G6cvm5p2Wvrpia5ujja6lPrUmsRz7s9oAZjWc_DW6Vrug/viewform?embedded=true');
+
+  window.removeEventListener('load', pageLoaded);
+}
+window.addEventListener('load', pageLoaded);
+// Loading screen end
